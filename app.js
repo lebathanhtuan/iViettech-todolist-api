390f4e7ba14567c3127de8a8c9abc4f7805b289f
Le Ba Thanh Tuan <le.ba.thanh.tuan@sun-asterisk.com>
2026-08-05 20:30:14 +0700

    Login Register

--- app.js
+++ app.js
@@ -2,53 +2,17 @@ import express from 'express'
 import path from 'path'
 import cors from 'cors'
 
-let taskList = []
+import userRoute from './routes/user.route.js'
+import taskRoute from './routes/task.route.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.static('publish'))
 app.use(express.json())
-app.use(cors())
 
-app.get('/tasks', (req, res) => {
-  res.json(taskList)
-})
-
-app.post('/tasks', (req, res) => {
-  const data = req.body
-  const newTask = {
-    id: new Date().getTime().toString(),
-    name: data.name,
-    description: data.description,
-  }
-  taskList.unshift(newTask)
-  res.json(newTask)
-})
-
-app.put('/tasks/:id', (req, res) => {
-  // Sửa dữ liệu trong taskList
-  const id = req.params.id
-  const data = req.body
-  const updatedTask = {
-    id: id,
-    name: data.name,
-    description: data.description,
-  }
-  taskList = taskList.map((task) => {
-    if (task.id === updatedTask.id) {
-      return updatedTask
-    }
-    return task
-  })
-  res.json(updatedTask)
-})
-
-app.delete('/tasks/:id', (req, res) => {
-  // Xóa dữ liệu trong taskList
-  const id = req.params.id
-  taskList = taskList.filter((task) => task.id !== id)
-  res.json({ id: id })
-})
+app.use('/users', userRoute)
+app.use('/tasks', taskRoute)
 
 app.listen(4000, () => {
   console.log('Chạy thành công')
